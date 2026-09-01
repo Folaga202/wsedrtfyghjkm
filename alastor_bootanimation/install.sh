450a5465095d4eba94272ba6f75f06dbad56f3f8
Folaga202 <le.b32@protonmail.com>
2026-09-02 00:08:18 +0200

    Add install.sh for Magisk module

---
+++ alastor_bootanimation/install.sh
@@ -0,0 +1,7 @@
+#!/system/bin/sh
+# This script is executed as a shell script
+# Placeholder for any custom installation logic if needed
+# For a simple bootanimation replacement, Magisk handles the system/product/media/ path automatically
+
+MODDIR=${0%/*}
+exit 0
